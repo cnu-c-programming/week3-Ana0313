b618e033d1975a1fae36a4cf9a3d214725b3da20
Ana0313 <rokafosh2@gmail.com>
2026-03-26 21:09:16 +0900

    Update 3-3.c

--- code/3-3.c
+++ code/3-3.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stdarg.h>
+
+int average(int count, ...) {
+    int sum = 0;
+    va_list list; 
+
+    va_start(list, count); 
+
+    
+    for (int i = 0; i < count; i++) {
+        sum += va_arg(list, int); 
+    }
+
+    va_end(list); 
+
+    return sum / count; 
+}
 
 int main() {
-
+    printf("%d\n", average(3, 10, 20, 30));
+    printf("%d\n", average(5, 10, 20, 30, 40, 50));
     return 0;
 }
